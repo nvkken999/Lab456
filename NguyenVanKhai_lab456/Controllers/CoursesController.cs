8ff0a638a6ec9377931081866e7fc0edb37520be
Administrator <Administrator@PC226>
2020-06-04 10:36:10 +0700

    comit7

--- NguyenVanKhai_lab456/Controllers/CoursesController.cs
+++ NguyenVanKhai_lab456/Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NguyenVanKhai_lab456.Models;
+using NguyenVanKhai_lab456.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,21 @@ namespace NguyenVanKhai_lab456.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+
         // GET: Courses
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
         }
     }
 }
