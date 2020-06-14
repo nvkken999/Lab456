5d9aa9009f74e1d185a424485e55026ceabd358b
nvkke <nvkke@DESKTOP-48BSC9B>
2020-06-14 12:24:39 +0700

    home3

---
+++ NguyenVanKhai_lab456/Controllers/AttendancesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NguyenVanKhai_lab456.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+    }
+}
