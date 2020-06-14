fe2b6c35362d0565ccc13ba217a1deadb6513cbe
nvkke <nvkke@DESKTOP-48BSC9B>
2020-06-14 13:29:41 +0700

    home4

--- NguyenVanKhai_lab456/Controllers/AttendancesController.cs
+++ NguyenVanKhai_lab456/Controllers/AttendancesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using NguyenVanKhai_lab456.DTOs;
+using NguyenVanKhai_lab456.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,29 @@ using System.Web.Http;
 
 namespace NguyenVanKhai_lab456.Controllers
 {
+    [Authorize]
     public class AttendancesController : ApiController
     {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend (AttendanceDto atendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Ateendances.Any(a => a.AttendeeId == userId && a.CourseID == atendanceDto.CourseId))
+                return BadRequest("The attendance alreary exists!");
+            var attendance = new Attendance
+            {
+                CourseID = atendanceDto.CourseId,
+                AttendeeId = userId
+            };
+          
+            _dbContext.Ateendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
     }
 }
